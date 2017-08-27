6186a823ea7bb8c256e5c77bbdf64b5f5f297797
Maleki <cameronjc2000@yahoo.ca>
2017-08-26 23:54:20 -0400

    small change removed extaneous task

--- gulpfile.js
+++ gulpfile.js
@@ -12,15 +12,3 @@ gulp.task('filter', function(){
         .pipe(f)
         .pipe(gulp.dest(`.\\Database\\Archived Scripts\\v01_00\\${newTag}_Current`));
 });
-
-gulp.task('filter_test', function(){
-    newTag = "v1.0.0.20";
-     var answer = JSON.parse('{"version":"v1.0.0.19","commitHash":"e3f286806|Some Guy|2017-08-24T19:22:59-04:00|fix","sqlFiles":["Database/Current Scripts/0101_DictionaryInsert_ManualTaskStatus.sql","Database/Current Scripts/5000_PhysicianInsert.sql","Database/Stored Procedures/AddReportPermissionExclude.sql","Database/Stored Procedures/AgreementToPaymentListForGrid.sql"]}');
-     var f = filter(['**/Current Scripts/*.sql'], { debug: true });
-
-     return gulp.src(answer.sqlFiles)
-        .pipe(gulp.dest(`.\\Database\\Archived Scripts\\v01_00\\${newTag}`))
-        .pipe(f)
-        .pipe(gulp.dest(`.\\Database\\Archived Scripts\\v01_00\\${newTag}_Current`));
-     
-});
